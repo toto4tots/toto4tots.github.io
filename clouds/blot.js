81752d2f824da96830b6528af5824a090a1be66f
Kyuri <toto4tot@math4totss-iMac.local>
2021-06-04 15:11:19 -0700

    edit

--- clouds/blot.js
+++ clouds/blot.js
@@ -15,13 +15,13 @@ class Blot {
 
 class BlotRandomSetting {
     // holds info of one shade 
-    constructor(x, y, radius, sdx = 10, sdy = 10, generation = 2, alpha = 2) {
+    constructor(x, y, radius, sdx = 10, sdy = 10, generation = 2, alpha = 4) {
         this.x = randomGaussian(x, sdx);
         this.y = randomGaussian(y, sdy);
         this.radius = randomGaussian(radius);
         this.numSides = Math.floor(randomGaussian(10));
         this.generation = Math.floor(randomGaussian(generation, 1));
-        this.alpha = randomGaussian(4, 4);
+        this.alpha = randomGaussian(alpha, 4);
     }
 }
 
